eb256f3a8162048e72213d981ebb1ab126e35c50
sbritt404 <sbrittain.prsvr@gmail.com>
2024-07-31 15:49:50 -0400

    controller/router finished

--- bookDatabase-BE/routes/bookRouter.js
+++ bookDatabase-BE/routes/bookRouter.js
@@ -3,20 +3,20 @@ const router = express.Router()
 
 const bookController = require('./product/controller/bookController')
 
-// router.get('/get-all-books', (req, res) => {
-//     bookController.getAllBooks((error, payload) => {
-//         if (error) {
-//             res.status(500).json({message: "Error", error: error})
-//         }else{
-//             res.json({message: 'Books found.', data: payload})
-//         }
-//     })
-// })
+router.get('/get-all-books', (req, res) => {
+    bookController.getAllBooks((err, payload) => {
+        if (err) {
+            res.status(500).json({message: "Error", error: err})
+        }else{
+            res.json({message: 'Books found.', data: payload})
+        }
+    })
+})
 
 router.post('/create-book', (req, res) => {
-    bookController.createBook(req.body, (error, payload) => {
-        if(error) {
-            res.status(500).json({message: "Error", error: error})
+    bookController.createBook(req.body, (err, payload) => {
+        if(err) {
+            res.status(500).json({message: "Error", error: err})
         }else{
             res.json({message: 'Book Created', data: payload})
         }
@@ -24,11 +24,23 @@ router.post('/create-book', (req, res) => {
 })
 
 router.put('/update-book/:id', (req, res) => {
-
+    bookController.updateBookByID(req.params.id, req.body, (err, payload) => {
+        if (err) {
+            res.status(500).json({ message: "Error", error: err })
+        } else {
+            res.json({ message: 'Book Created.', data: payload })
+        }
+    })
 })
 
 router.delete('delete-book/:id', (req, res) => {
-
+    bookController.updateBookByID(req.params.id, (err, payload) => {
+        if (err) {
+            res.status(500).json({ message: "Error", error: err})
+        } else {
+            res.json({ message: 'Book Deleted.', data: payload })
+        }
+    })
 })
 
 module.exports = router
--- bookDatabase-BE/routes/product/controller/bookController.js
+++ bookDatabase-BE/routes/product/controller/bookController.js
@@ -1,15 +1,15 @@
 const Book = require('../model/Book')
 
 module.exports = {
-    // getAllBooks: function (callback) {
-    //     Book.find({})
-    //     .then(payload => {
-    //         callback(null, payload)
-    //     })
-    //     .catch(error => {
-    //         callback(error,null)
-    //     })
-    // },
+    getAllBooks: function (callback) {
+        Book.find({})
+        .then(payload => {
+            callback(null, payload)
+        })
+        .catch(error => {
+            callback(error,null)
+        })
+    },
 
     createBook: function (body, callback) {
         const {
@@ -29,7 +29,25 @@ module.exports = {
         .catch(error => {
             callback(error, null)
         })
-    }
+    },
     //updateBook(ByID)?
+    updateBookByID: function (id, body, callback) {
+        Book.findByIdAndUpdate(id, body, { new: true })
+            .then(updatedPayload => {
+                callback(null, updatedPayload)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
+    },
     //deleteBook(ByID)?
+    deleteBookByID: function (id, callback) {
+        Book.findByIdAndDelete(id)
+            .then(deletedBook => {
+                callback(null, deletedBook)
+            })
+            .catch(error => {
+                callback(error, null)
+            })
+    }
 }
